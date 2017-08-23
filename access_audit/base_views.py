359a5e1064a02eb620a6b006b20bf99f280b38b4
Ben Golder <bgolder@codeforamerica.org>
2017-08-23 15:12:00 -0700

    WIP building out base view

---
+++ access_audit/base_views.py
@@ -0,0 +1,79 @@
+from rest_framework import exceptions, status
+
+
+def exception_handler(exc, context):
+    """
+    Returns the response that should be used for any given exception.
+
+    By default we handle the REST framework `APIException`, and also
+    Django's built-in `Http404` and `PermissionDenied` exceptions.
+
+    Any unhandled exceptions may return `None`, which will cause a 500 error
+    to be raised.
+    """
+    if isinstance(exc, exceptions.APIException):
+        headers = {}
+        if getattr(exc, 'auth_header', None):
+            headers['WWW-Authenticate'] = exc.auth_header
+        if getattr(exc, 'wait', None):
+            headers['Retry-After'] = '%d' % exc.wait
+
+        if isinstance(exc.detail, (list, dict)):
+            data = exc.detail
+        else:
+            data = {'detail': exc.detail}
+
+        set_rollback()
+        return Response(data, status=exc.status_code, headers=headers)
+
+    elif isinstance(exc, Http404):
+        msg = _('Not found.')
+        data = {'detail': six.text_type(msg)}
+
+        set_rollback()
+        return Response(data, status=status.HTTP_404_NOT_FOUND)
+
+    elif isinstance(exc, PermissionDenied):
+        msg = _('Permission denied.')
+        data = {'detail': six.text_type(msg)}
+
+        set_rollback()
+        return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    return None
+
+
+class PermissionsBaseView:
+
+    def handle_exception(self, exc):
+        """
+        Handle any exception that occurs, by returning an appropriate response,
+        or re-raising the error.
+        """
+        if isinstance(exc, (exceptions.NotAuthenticated,
+                            exceptions.AuthenticationFailed)):
+            exc.status_code = status.HTTP_403_FORBIDDEN
+        # here we will delegate to Django's exception handling
+        raise exc
+
+    def dispatch(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.request = request
+        try:
+            self.initial(request, *args, **kwargs)
+
+            # Get the appropriate handler method
+            if request.method.lower() in self.http_method_names:
+                handler = getattr(self, request.method.lower(),
+                                  self.http_method_not_allowed)
+            else:
+                handler = self.http_method_not_allowed
+
+            response = handler(request, *args, **kwargs)
+
+        except Exception as exc:
+            response = self.handle_exception(exc)
+        self.response = response
+        response.view = self
+        return response
